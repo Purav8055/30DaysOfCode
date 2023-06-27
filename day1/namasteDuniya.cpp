5eb06d9b63e70bf9304449d42056315dceae3686
puravKhurana <121746793+Purav8055@users.noreply.github.com>
2023-06-27 14:06:54 +0530

    Create namasteDuniya.cpp

---
+++ day1/namasteDuniya.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int c;
+    cout << "value of a is:" << a << "\n";
+    cout << "a has " << a << " as its value\n";
+    cout << "Namaste Duniya\n";
+    int b = -3;
+    cout << !b;
+}
